7655c102d9e5081e4c0ba16e997a906b32b91177
Neotrlx <luis.e.sanmartin.e@gmail.com>
2022-11-30 16:42:51 -0600

    se corrigio el commit y se sube la ventana Movil_Detail_Activity.xml

--- app/src/main/java/com/example/movieapp/Movil_Detail_Activity.kt
+++ app/src/main/java/com/example/movieapp/Movil_Detail_Activity.kt
@@ -3,9 +3,10 @@ package com.example.movieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Movil_Detail_Luis_Enrique : AppCompatActivity() {
+class Movil_Detail_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movil_detail_luis_enrique)
+        setContentView(R.layout.activity_movil_detail)
     }
+    //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
 }
