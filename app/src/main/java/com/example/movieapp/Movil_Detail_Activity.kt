71295f88c873b8c590b04d0af8142bf5773f6e28
Neotrlx <luis.e.sanmartin.e@gmail.com>
2022-11-29 20:00:52 -0600

    Se añade la ventana de movil detail

---
+++ app/src/main/java/com/example/movieapp/Movil_Detail_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movil_Detail_Luis_Enrique : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movil_detail_luis_enrique)
+    }
+}
