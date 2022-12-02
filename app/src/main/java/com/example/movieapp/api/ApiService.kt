a40b69316b7595b3078174ec87494c37a2acbe96
omar.gutierrez2 <omar.gutierrez@umvel.com>
2022-12-01 18:19:28 -0600

    Add movie api service

---
+++ app/src/main/java/com/example/movieapp/api/ApiService.kt
@@ -0,0 +1,20 @@
+package com.example.movieapp.api
+
+import com.example.movieapp.MovieApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+const val BASE_URL = "https://api.themoviedb.org/3/"
+
+object ApiService {
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val service: MovieApi by lazy {
+        retrofit.create(MovieApi::class.java)
+    }
+
+}
