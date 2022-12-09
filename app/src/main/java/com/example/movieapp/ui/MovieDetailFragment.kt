53e4df51f4d608765a0972381b4ea2825a660e1f
Neotrlx <luis.e.sanmartin.e@gmail.com>
2022-12-08 18:12:45 -0600

    se agrega conexion de los fragment moviedetail y movielist para mostrar informacion correspondiente a cada pelicula

--- app/src/main/java/com/example/movieapp/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/MovieDetailFragment.kt
@@ -4,16 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.movieapp.R
+import com.example.movieapp.toMovieUrl
 
 class MovieDetailFragment : Fragment() {
+
+    lateinit var stringTituloPelicula: String
+    lateinit var stringImagenFondo:String
+    lateinit var stringImagenPrincipal:String
+    lateinit var stringDescripcionPelicula:String
+    lateinit var stringFechaHora:String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let { bundle ->
+            stringTituloPelicula = bundle.getString("title").toString()
+            stringImagenFondo = bundle.getString("backdrop_path").toString()
+            stringImagenPrincipal = bundle.getString("poster_path").toString()
+            stringDescripcionPelicula = bundle.getString("overview").toString()
+            stringFechaHora = bundle.getString("release_date").toString()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.activity_movil_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tituloPelicula = view.findViewById<TextView>(R.id.titulo_pelicula)
+        val imagenFondo = view.findViewById<ImageView>(R.id.imagen_fondo)
+        val imagenPrincipal = view.findViewById<ImageView>(R.id.imagen_principal)
+        val descripcionPelicula = view.findViewById<TextView>(R.id.descripcionpelicula)
+        val fechaHora = view.findViewById<TextView>(R.id.fecha_hora)
+
+        tituloPelicula.text = stringTituloPelicula
+        descripcionPelicula.text = stringDescripcionPelicula
+        fechaHora.text = stringFechaHora
+        Glide.with(requireContext())
+            .load(stringImagenFondo?.toMovieUrl())
+            .into(imagenFondo)
+        Glide.with(requireContext())
+            .load(stringImagenPrincipal.toMovieUrl())
+            .into(imagenPrincipal)
     }
 
 }
--- app/src/main/java/com/example/movieapp/ui/fragments/movie_list_by_category/MovieListByCategoryFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/fragments/movie_list_by_category/MovieListByCategoryFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
@@ -41,7 +42,13 @@ class MovieListByCategoryFragment : Fragment() {
         val rvByCategoriesMovies = view?.findViewById<RecyclerView>(R.id.rvCategoryListMovies)
         rvByCategoriesMovies?.layoutManager = GridLayoutManager(context, 2)
         val adapter = AllMoviesAdapter(list) {
-            Toast.makeText(requireContext(), "Add event to navigate", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_movieListByCategoryFragment_to_movieDetailFragment,
+                bundleOf(
+                    "title" to it.title,
+                    "backdrop_path" to it.backdrop_path,
+                    "poster_path" to it.poster_path,
+                    "overview" to it.overview,
+                    "release_date" to it.release_date))
         }
         rvByCategoriesMovies?.adapter = adapter
     }
